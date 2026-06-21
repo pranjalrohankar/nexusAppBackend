0d91369a97e1c8e56814c31cfc1bdeded4ae2fe0
ruchitajadhav27 <jadhavruchita27@gmail.com>
2026-06-22 00:03:21 +0530

    add database and successfully done with loging pages of admin student teacher with smtp services send login credentials in email with perfect ui

---
+++ src/main/java/com/nexus/backend/dto/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.nexus.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String role;
+    private String name;
+    private String email;
+}
