edd8385b5a9cb3451554a9ec98aac54197e1e604
ruchitajadhav27 <jadhavruchita27@gmail.com>
2026-06-25 17:41:42 +0530

    solve conflits from dev-aditya branch

--- src/main/java/com/nexus/backend/repository/BatchRepository.java
+++ src/main/java/com/nexus/backend/repository/BatchRepository.java
@@ -2,10 +2,6 @@ package com.nexus.backend.repository;
 
 import com.nexus.backend.model.Batch;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
-=======
->>>>>>> eb687a352d9d49f8c40040db1ba4a1ada5add4d9
 
 public interface BatchRepository extends JpaRepository<Batch, Long> {
 
