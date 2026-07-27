b9e01c33ad5b9b7d50a31576225a4bfd7fe601ab
ruchitajadhav27 <jadhavruchita27@gmail.com>
2026-07-27 11:03:58 +0530

    new chnages in api

--- src/main/java/com/nexus/backend/dto/BatchDto.java
+++ src/main/java/com/nexus/backend/dto/BatchDto.java
@@ -15,41 +15,33 @@ public class BatchDto{
     private LocalDate endDate;
     private List<ClassDay> classDays;
     private BatchStatus status;
-
     private String classTimings;
     private String duration;
 
-    public String getBatchName() {
-        return batchName;
-    }
-    public String getselectCourse() {
-        return selectCourse;
-    }
-    public String getInstructor() {
-        return instructor;
-    }
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-    public List<ClassDay> getClassDays() {
-        return classDays;
-    }
-    public BatchStatus getStatus() {
-        return status;
-    }
-    public String getClassTimings() {
-        return classTimings;
-    }
-    public void setClassTimings(String classTimings) {
-        this.classTimings = classTimings;
-    }
-    public String getDuration() {
-        return duration;
-    }
-    public void setDuration(String duration) {
-        this.duration = duration;
-    }
+    public String getBatchName() { return batchName; }
+    public void setBatchName(String batchName) { this.batchName = batchName; }
+
+    public String getSelectCourse() { return selectCourse; }
+    public void setSelectCourse(String selectCourse) { this.selectCourse = selectCourse; }
+
+    public String getInstructor() { return instructor; }
+    public void setInstructor(String instructor) { this.instructor = instructor; }
+
+    public LocalDate getStartDate() { return startDate; }
+    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
+
+    public LocalDate getEndDate() { return endDate; }
+    public void setEndDate(LocalDate endDate) { this.endDate = endDate; }
+
+    public List<ClassDay> getClassDays() { return classDays; }
+    public void setClassDays(List<ClassDay> classDays) { this.classDays = classDays; }
+
+    public BatchStatus getStatus() { return status; }
+    public void setStatus(BatchStatus status) { this.status = status; }
+
+    public String getClassTimings() { return classTimings; }
+    public void setClassTimings(String classTimings) { this.classTimings = classTimings; }
+
+    public String getDuration() { return duration; }
+    public void setDuration(String duration) { this.duration = duration; }
 }
